ead59783584ec03e17e0812a5d4442801efa51ad
Christopher Kimbell <christopher.kimbell@dnv.com>
2024-09-11 14:30:48 +0200

    repro code

---
+++ OpenApi.Web/Controllers/SchemaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenApi.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class SchemaController : ControllerBase
+    {
+        [HttpGet(Name = "Echo")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(EchoData))]
+        public IActionResult GetString([FromBody] EchoData body)
+        {
+            return Ok(body);
+        }
+    }
+
+    public class EchoData
+    {
+        public EchoData2? Level2 { get; set; }
+    }
+
+    public class EchoData2
+    {
+        public EchoData3? Level3 { get; set; }
+    }
+
+    public class EchoData3
+    {
+        public string Something { get; set; } = default!;
+    }
+}
---
+++ OpenApi.Web/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace OpenApi.Web
+{
+#pragma warning disable CA1052 // Static holder types should be Static or NotInheritable
+    // used by tests
+    public class Program
+#pragma warning restore CA1052 // Static holder types should be Static or NotInheritable
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddOpenApi("Microsoft", o =>
+            {
+                o.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_0;
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwaggerUI(o =>
+                {
+                    o.RoutePrefix = "docs";
+                    o.SwaggerEndpoint("Microsoft.json", "Microsoft version (TODO)");
+                });
+                app.MapOpenApi("/docs/{documentName}.json");
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
